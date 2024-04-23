ecd7358167fb8743d97a1e25197127f563a553cb
Prynxle <zespiritu23a-0089@olopsc.edu.ph>
2024-04-23 16:01:08 +0800

    0.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+import 'pages/login_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: Login(),
+    );
   }
 }
